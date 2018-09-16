b9171487deb3e2c6e8a18408e7f3add4e193a3e4
root <root@Arthur.localdomain>
2018-09-15 20:19:50 -0700

    guessing game

---
+++ app.js
@@ -0,0 +1,54 @@
+                 'use strict';
+            var question =prompt('is my name arthur?');
+            var properName=question.toUpperCase();
+         if(properName === 'y' || properName === 'yes')
+           {
+               console.log(properName)
+             alert('hi that is my name');
+           }
+           else if(properName === 'n' || properName === 'no')
+           {
+               alert('that is not my name');
+           }
+                          
+            var questionTwo =prompt('do i have horse?');
+            var properTwo=questionTwo.toUpperCase();
+         if(properTwo === 'y' || properTwo === 'yes')
+           {
+               console.log(properTwo)
+             alert('i have a horse');
+           }
+           else if(properTwo === 'n' || properTwo === 'no')
+           {
+               alert('you are wrong');
+           }
+         
+            var questionThree =prompt('do i have a dog?');
+            var properThree=questionThree.toUpperCase();
+            if(properThree === 'y' || properThree === 'yes')
+           {
+               console.log(properThree)
+             alert('i have a dog');
+           }
+           else if(properThree === 'n' || properThree === 'no')
+           {
+               alert('you are wrong');
+           }
+           
+               var questionFour =prompt('have i flown a plane');
+            var properFour=questionFour.toUpperCase();
+            if(properFour === 'y' || properFour === 'yes')
+           {
+               console.log(properFour)
+             alert('nice try ');
+           }
+           else if(properThree === 'n' || properThree === 'no')
+           {
+               alert('you are right');
+           }
+           
+           
+           
+         
+    
+     
